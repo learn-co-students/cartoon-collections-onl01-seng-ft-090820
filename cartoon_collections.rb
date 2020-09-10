938f188977dddbec9cfe360c3978fe7c2526ccbc
Dasha Lary <dk2800@nyu.edu>
2020-09-10 23:00:28 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfarray)
+  dwarfarray.each_with_index { |dwarf, index| puts "#{index + 1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteerarray)
+  planeteerarray.map { |planeteer| planeteer.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(callsarray)
+  if callsarray.any? { |word| word.length > 4 }
+  true
+else
+false
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(stringarray)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if stringarray.include?("cheddar") || stringarray.include?("gouda") || stringarray.include?("camembert")
+  stringarray.find { |cheese| cheese_types.include?(cheese) }
+    
+  else 
+    return nil
+end
 end
