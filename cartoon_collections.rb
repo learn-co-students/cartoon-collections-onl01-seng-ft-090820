6cd1a9c6d4f15946d95c17c6d6e87c1a0bedd117
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-21 21:32:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
+dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(dwarfs)
-  dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
   dwarfs.each_with_index do | name , index |
     puts "#{index + 1} #{name}"
   end
